0bc2b3a4b82551757ffb5c9b437491a1899220d9
coderYZW <15226569565@163.com>
2019-09-10 23:58:41 +0800

    更改项目结构

--- Test.py
+++
@@ -1,7 +0,0 @@
-import requests
-from requests.packages import urllib3
-
-urllib3.disable_warnings()
-# response=requests.get("https://www.12306.cn",verify=False)
-response=requests.get("https://www.12306.cn")
-print(response.status_code)
